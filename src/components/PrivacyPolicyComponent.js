2f64e7b202a86fdaf1b031c8f0f64e66002fcae0
Satya Deva Ankey <satyadeva.ankey@gmail.com>
2020-11-30 14:46:37 -0500

    Add profile and privacy Policy component

---
+++ src/components/PrivacyPolicyComponent.js
@@ -0,0 +1,154 @@
+import React from 'react';
+
+class PrivacyPolicyComponent extends React.Component {
+    render() {
+        return (
+            <div class="container">
+
+                <a href="/signIn"></a>
+                <div>
+                    <h1 style={{"text-align": "center"}}>Privacy Policy</h1>
+                </div>
+
+                <div>
+                    <p>
+                        Thank you for choosing to be part of our community at Job Pool ("Company",
+                        "we",
+                        "us", "our"). We are committed to protecting your personal information and
+                        your
+                        right to privacy. If you have any questions or concerns about this privacy
+                        notice, or our practices with regards to your personal information, please
+                        contact us at jobpool@gmail.com.
+                    </p>
+                </div>
+                <div>
+                    <div>
+                        <h3>What Information do we collect?</h3>
+                    </div>
+                    <div>
+                        <p>
+                            We collect personal information that you voluntarily provide to us when
+                            you register on the Website, express an interest in obtaining
+                            information about us or our products and Services, when you participate
+                            in activities on the Website or otherwise when you contact us.
+
+                            The personal information that we collect depends on the context of your
+                            interactions with us and the Website, the choices you make and the
+                            products and features you use. The personal information we collect may
+                            include the following:
+
+                            Personal Information Provided by You. We collect names; phone numbers;
+                            email addresses; usernames; passwords; contact preferences; contact or
+                            authentication data; mailing addresses; job titles; and other similar
+                            information.
+
+                            All personal information that you provide to us must be true, complete
+                            and accurate, and you must notify us of any changes to such personal
+                            information.t data we collect
+                        </p>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <h3>How do we use your information?</h3>
+                    </div>
+                    <div>
+                        <p>
+                            We use personal information collected via our Website for a variety of
+                            business purposes described below. We process your personal information
+                            for these purposes in reliance on our legitimate business interests, in
+                            order to enter into or perform a contract with you, with your consent,
+                            and/or for compliance with our legal obligations. We indicate the
+                            specific processing grounds we rely on next to each purpose listed
+                            below.
+
+                            We use the information we collect or receive:
+
+                            Fulfill and manage your orders. We may use your information to fulfill
+                            and manage your orders, payments, returns, and exchanges made through
+                            the Website.
+
+                            Administer prize draws and competitions. We may use your information to
+                            administer prize draws and competitions when you elect to participate in
+                            our competitions.
+
+                            To deliver and facilitate delivery of services to the user. We may use
+                            your information to provide you with the requested service.
+
+                            To respond to user inquiries/offer support to users. We may use your
+                            information to respond to your inquiries and solve any potential issues
+                            you might have with the use of our Services.
+
+                        </p>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <h3>Will your information be shared with anyone?</h3>
+                        <p>
+                            We may process or share your data that we hold based on the following
+                            legal basis:
+                            Consent: We may process your data if you have given us specific consent
+                            to use your personal information for a specific purpose.
+                            Legitimate Interests: We may process your data when it is reasonably
+                            necessary to achieve our legitimate business interests.
+                            Performance of a Contract: Where we have entered into a contract with
+                            you, we may process your personal information to fulfill the terms of
+                            our contract.
+                            Legal Obligations: We may disclose your information where we are legally
+                            required to do so in order to comply with applicable law, governmental
+                            requests, a judicial proceeding, court order, or legal process, such as
+                            in response to a court order or a subpoena (including in response to
+                            public authorities to meet national security or law enforcement
+                            requirements).
+                            Vital Interests: We may disclose your information where we believe it is
+                            necessary to investigate, prevent, or take action regarding potential
+                            violations of our policies, suspected fraud, situations involving
+                            potential threats to the safety of any person and illegal activities, or
+                            as evidence in litigation in which we are involved.
+                            More specifically, we may need to process your data or share your
+                            personal information in the following situations:
+                            Business Transfers. We may share or transfer your information in
+                            connection with, or during negotiations of, any merger, sale of company
+                            assets, financing, or acquisition of all or a portion of our business to
+                            another company
+                        </p>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <h3>Do we use cookies and other tracking technologies?</h3>
+                        <p>
+
+                        </p>
+                    </div>
+                    We may use cookies and similar tracking technologies (like web beacons and
+                    pixels) to access or store information. Specific information about how we use
+                    such technologies and how you can refuse certain cookies is set out in our
+                    Cookie Notice.
+                </div>
+                <div>
+                    <div>
+                        <h3>How can you contact us about this notice?</h3>
+                        <p>
+                            If you have questions or comments about this notice, you may email us at jobpool@gmail.com or by post to:
+
+                            Job Pool
+                            360, HUNTINGTON AVENUE
+                            Boston, MA 02114
+                            United States
+                        </p>
+                    </div>
+                </div>
+                <div>
+                    <p>
+                        To continue using our website, accept our privacy policy.
+                        Alternatively, you may choose to decline our privacy policy.
+                    </p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default PrivacyPolicyComponent;
