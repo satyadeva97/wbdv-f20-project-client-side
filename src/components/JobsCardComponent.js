4b7e73161d9839a535eea3ab508127cad8ea3222
jayeshlalwani <jayeshlalwani9294@gmail.com>
2020-12-01 00:11:57 -0500

    minor changes in jobsCardComponent

--- src/components/JobsCardComponent.js
+++ src/components/JobsCardComponent.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import "./JobComponent.scss"
+import React from "react";
+import "./JobComponent.scss";
 class JobsCardComponent extends React.Component {
-    render() {
-        return (
-            <a href={this.props.job.url}>
-                <li className="flex-item">
-                    <div className="flex-col-container">
-                        <div className="company-logo">
-                            <img src={this.props.job.company_logo} alt="Card image cap"/>
-                        </div>
-                        <div className="job-designation">
-                            {this.props.job.title}
-                        </div>
-                        <div className="company-name">
-                            {this.props.job.company}
-                        </div>
-                    </div>
-                </li>
-            </a>
-        )
-    }
+  render() {
+    return (
+      <li className="flex-item p-0 b-0">
+        <div className="flex-col-container">
+          <div className="company-logo d-flex justify-content-center">
+            <img src={this.props.job.company_logo} alt="" />
+          </div>
+          <a href={`/job/${this.props.job.id}`}>
+            <div className="job-designation">{this.props.job.title}</div>
+          </a>
+          <a href={this.props.job.company_url} target="_blank" rel="noreferrer">
+            <div className="company-name">{this.props.job.company}</div>
+          </a>
+        </div>
+      </li>
+    );
+  }
 }
 export default JobsCardComponent;
