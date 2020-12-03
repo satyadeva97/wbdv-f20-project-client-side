a50c2913034bb894680eee7a3429422e6f6d115c
Satyadeva97 <satyadeva.ankey@gmail.com>
2020-12-03 12:49:50 -0500

    Correct navigation links

--- src/components/core/FooterComponent.js
+++ src/components/core/FooterComponent.js
@@ -1,25 +1,19 @@
-import React from 'react';
-import "./footerComponent.scss"
+import React from "react";
+import "./footerComponent.scss";
 
-class FooterComponent extends React.Component{
-    render() {
-        return (
-            <div className="container">
-            <footer className="page-footer font-small blue">
-                <div className="row">
-                <div >
-                    <a href="/privacy">Privacy Policy</a>
-                </div>
-                <div>
-                    About
-                </div>
-                </div>
-            </footer>
-            </div>
-
-
-        )
-    }
+class FooterComponent extends React.Component {
+  render() {
+    return (
+      <nav className="navbar container navbar-light bg-light">
+        <div className="row">
+          <div className="col-2">
+            <a href="/privacy">Privacy Policy</a>
+          </div>
+          <div className="col-1">About</div>
+        </div>
+      </nav>
+    );
+  }
 }
 
 export default FooterComponent;
--- src/components/job/JobsComponent.js
+++ src/components/job/JobsComponent.js
@@ -1,14 +1,28 @@
 import React from "react";
 import "./JobComponent.scss";
 import JobsCardComponent from "./JobsCardComponent";
+import JobSearchComponent from "./JobSearchComponent";
 class JobsComponent extends React.Component {
+  onJobSearch = ({ keyword = "", location = "" }) => {
+    this.props.history.push(
+      `/search/keyword/${keyword ? keyword : "%20"}/location/${
+        location ? location : "%20"
+      }/`
+    );
+  };
+
   render() {
     return (
       <div>
+        <JobSearchComponent onSearch={this.onJobSearch} />
+        <div className="text-center">
+          {(this.props.search.keyword || this.props.search.keyword) &&
+            `Showing results for Keyword:${this.props.search.keyword} Location:${this.props.search.location}`}
+        </div>
         {this.props.jobs.length === 0 && <h5>No jobs to show</h5>}
 
         {this.props.jobs && (
-          <ul className="row flex-container wrap ">
+          <ul className="row flex-container wrap no-gutters">
             {this.props.jobs.map((job) => {
               return <JobsCardComponent key={job.id} job={job} />;
             })}
