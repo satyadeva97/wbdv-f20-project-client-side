39938740adbd65f029895d8d91d39decc64a4149
Satyadeva97 <satyadeva.ankey@gmail.com>
2020-12-08 11:59:44 -0500

    add trackpromise in services

--- src/services/JobService.js
+++ src/services/JobService.js
@@ -1,26 +1,34 @@
+import { trackPromise } from "react-promise-tracker";
+
 const proxyUrl = "https://cors-anywhere.herokuapp.com/";
 
 export const getAllJobs = (keyword, location) => {
-  return fetch(
-    `${proxyUrl}https://jobs.github.com/positions.json?page=1&description=${keyword}&location=${location}`
-  ).then(function (response) {
-    return response.json();
-  });
+  return trackPromise(
+    fetch(
+      `${proxyUrl}https://jobs.github.com/positions.json?page=1&description=${keyword}&location=${location}`
+    ).then(function (response) {
+      return response.json();
+    })
+  );
 };
 
 export const getJobsByLocation = (loc) => {
-  return fetch(
-    `${proxyUrl}https://jobs.github.com/positions.json?location=${loc}`
-  ).then(function (response) {
-    return response.json();
-  });
+  return trackPromise(
+    fetch(
+      `${proxyUrl}https://jobs.github.com/positions.json?location=${loc}`
+    ).then(function (response) {
+      return response.json();
+    })
+  );
 };
 
 export const getJobDetailsById = (id) => {
-  return fetch(`${proxyUrl}https://jobs.github.com/positions/${id}.json`).then(
-    (response) => {
-      return response.json();
-    }
+  return trackPromise(
+    fetch(`${proxyUrl}https://jobs.github.com/positions/${id}.json`).then(
+      (response) => {
+        return response.json();
+      }
+    )
   );
 };
 
