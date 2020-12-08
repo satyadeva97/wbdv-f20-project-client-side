12d7234e99d9ff0c0313008e22816bc289efd475
Satyadeva97 <satyadeva.ankey@gmail.com>
2020-12-08 12:03:21 -0500

    add nomatch and warning components

---
+++ src/components/generic/NoMatch.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+class NoMatchComponent extends React.Component {
+  render() {
+    return (
+      <div className="jumbotron">
+        <h1 className="display-4">This is not a valid route</h1>
+        <p className="lead">
+          Click here to go to <Link to="/">HOME PAGE</Link>
+        </p>
+      </div>
+    );
+  }
+}
+export default NoMatchComponent;
