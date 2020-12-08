b17f79477a7433ba99d9f357d36cd40250b40752
Satyadeva97 <satyadeva.ankey@gmail.com>
2020-12-08 12:00:26 -0500

    style changes

--- src/containers/RecruiterContainer.js
+++ src/containers/RecruiterContainer.js
@@ -1,15 +1,36 @@
 import React from "react";
 import HeaderComponent from "../components/core/HeaderComponent";
+import RecruiterHome from "../components/homepage/RecruiterHome";
 import JobsPostComponent from "../components/job/JobPostComponent";
+import { getAllJobs } from "../services/JobService";
 import "./RecruiterContainer.scss";
 
 class RecruiterContainer extends React.Component {
+  componentDidMount() {
+    if (!this.props.postJob) {
+      this.getJobs({ keyword: "", location: "" });
+    }
+  }
+
+  state = {
+    jobs: [],
+  };
+
+  getJobs = async (search) => {
+    const jobs = await getAllJobs(search.keyword, search.location);
+    this.setState({ jobs: jobs });
+  };
+
   render() {
     return (
-      <div className="recuiter-container">
-        <HeaderComponent onSearch={this.onJobSearch} />
+      <div className="container-fluid recuiter-container">
+        <HeaderComponent />
 
-        {this.props.postJob && <JobsPostComponent />}
+        {this.props.postJob ? (
+          <JobsPostComponent />
+        ) : (
+          <RecruiterHome jobs={this.state.jobs} />
+        )}
       </div>
     );
   }
